41f64db05672aca06f9c463e9182bc61c6483003
Guy Barker (CELA) <guybark@microsoft.com>
2020-07-08 10:36:16 -0700

    Add project files.

---
+++ WinFormsFocusVisible/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace WinFormsFocusVisible
+{
+    partial class FormFocusVisible
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonStandard = new System.Windows.Forms.Button();
+            this.buttonCustom = new WinFormsFocusVisible.MyButton();
+            this.myUserControl = new WinFormsFocusVisible.MyUserControl();
+            this.buttonClose = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonStandard
+            // 
+            this.buttonStandard.Location = new System.Drawing.Point(37, 80);
+            this.buttonStandard.Name = "buttonStandard";
+            this.buttonStandard.Size = new System.Drawing.Size(149, 77);
+            this.buttonStandard.TabIndex = 0;
+            this.buttonStandard.Text = "&Standard";
+            this.buttonStandard.UseVisualStyleBackColor = true;
+            // 
+            // buttonCustom
+            // 
+            this.buttonCustom.Location = new System.Drawing.Point(226, 80);
+            this.buttonCustom.Name = "buttonCustom";
+            this.buttonCustom.Size = new System.Drawing.Size(149, 77);
+            this.buttonCustom.TabIndex = 1;
+            this.buttonCustom.Text = "&Custom";
+            this.buttonCustom.UseVisualStyleBackColor = true;
+            // 
+            // myUserControl
+            // 
+            this.myUserControl.Location = new System.Drawing.Point(415, 80);
+            this.myUserControl.Name = "myUserControl";
+            this.myUserControl.Size = new System.Drawing.Size(149, 77);
+            this.myUserControl.TabIndex = 2;
+            // 
+            // buttonClose
+            // 
+            this.buttonClose.Location = new System.Drawing.Point(633, 367);
+            this.buttonClose.Name = "buttonClose";
+            this.buttonClose.Size = new System.Drawing.Size(155, 71);
+            this.buttonClose.TabIndex = 3;
+            this.buttonClose.Text = "&Close";
+            this.buttonClose.UseVisualStyleBackColor = true;
+            this.buttonClose.Click += new System.EventHandler(this.buttonClose_Click);
+            // 
+            // FormFocusVisible
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonClose);
+            this.Controls.Add(this.myUserControl);
+            this.Controls.Add(this.buttonCustom);
+            this.Controls.Add(this.buttonStandard);
+            this.MaximizeBox = false;
+            this.Name = "FormFocusVisible";
+            this.Text = "Keyboard focus visuals demo";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonStandard;
+        private MyButton buttonCustom;
+        private MyUserControl myUserControl;
+        private System.Windows.Forms.Button buttonClose;
+    }
+}
+
---
+++ WinFormsFocusVisible/Form1.cs
@@ -0,0 +1,134 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+// This demo app shows different approaches to rendering keyboard focus visuals
+// in a WinForms app. By default, standard controls would be used in the app, 
+// which would mean that such custom rendering actions is not required.
+
+namespace WinFormsFocusVisible
+{
+    public partial class FormFocusVisible : Form
+    {
+        public FormFocusVisible()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+
+    // This button demonstrates the use of DrawFocusRectangle() 
+    // to render keyboard focus visuals.
+    public class MyButton : Button
+    {
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            // For this demo, simply fill the button bounds with a color.
+            e.Graphics.FillRectangle(Brushes.LightGreen, 
+                0, 0, this.Width, this.Height);
+
+            // Given that this method is written to deliberately prevent
+            // the keyboard focus visuals to be shown on the button by 
+            // default, take action now to force the keyboard focus visuals 
+            // to be rendered if the control has focus.
+            if (this.Focused)
+            {
+                // Render the feedback a little in from the egde of the button.
+                Rectangle rectFocus = this.ClientRectangle;
+                rectFocus.Inflate(-2, -2);
+
+                // Without this call, the focus visuals will not appear.
+                ControlPaint.DrawFocusRectangle(e.Graphics, rectFocus);
+            }
+        }
+    }
+
+    // This class demonstrates fully custom rendering of keyboard focus visuals.
+    // Note: The DrawFocusRectangle() called above will work on UserControl here,
+    // just as it does with the Button. But this UserControl intentionally doesn't 
+    // use DrawFocusRectangle(), in order to demo another approach, in case for 
+    // some reason your control doesn't have access to DrawFocusRectangle().
+
+    public class MyUserControl : UserControl
+    {
+        private Pen focusPenForeground;
+        private Pen focusPenBackground;
+        private bool myCustomControlHasFocus;
+
+        public MyUserControl()
+        {
+            // Create pens for the focus visuals which will be guaranteed to
+            // have strong contrast against the background of the control,
+            // regardess of the active theme.
+            CreateKeyboardFocusVisualsPens();
+
+            // Set up an event handler to be notified when the active theme 
+            // changes. For example, when the customer selects a high contrast theme.
+            SystemEvents.UserPreferenceChanged += SystemEvents_UserPreferenceChanged;
+
+            // We'll keep track of whether the control has focus.
+            this.GotFocus += MyUserControl_GotFocus;
+            this.LostFocus += MyUserControl_LostFocus;
+        }
+
+        private void SystemEvents_UserPreferenceChanged(object sender, UserPreferenceChangedEventArgs e)
+        {
+            CreateKeyboardFocusVisualsPens();
+        }
+
+        private void CreateKeyboardFocusVisualsPens()
+        {
+            // The keyboard focus visuals must always have contrast against 
+            // its surroundings. This can be achieved by having adjacent 
+            // inner and outer rects, using a foreground and a background
+            // color from the active theme. Alternatively, as shown here,
+            // the two contrasting color could be used in a dashed pattern.
+
+            this.focusPenForeground = new Pen(SystemColors.ControlText);
+            this.focusPenForeground.DashPattern = new float[] { 1, 1 };
+
+            this.focusPenBackground = new Pen(SystemColors.Control);
+        }
+
+        private void MyUserControl_GotFocus(object sender, EventArgs e)
+        {
+            this.myCustomControlHasFocus = true;
+
+            this.Refresh();
+        }
+
+        private void MyUserControl_LostFocus(object sender, EventArgs e)
+        {
+            this.myCustomControlHasFocus = false;
+
+            this.Refresh();
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            // For this demo, simply fill the control bounds with a color.
+            e.Graphics.FillRectangle(Brushes.LightGreen,
+                0, 0, this.Width, this.Height);
+
+            // For this example, say for some reason DrawFocusRectangle()
+            // is not available, and custom keyboard focus visuals are
+            // required.
+            if (this.myCustomControlHasFocus)
+            {
+                // Render the feedback a little in from the egde of the button.
+                Rectangle rectFocus = new Rectangle(
+                    0, 0, this.Width - 1, this.Height - 1);
+                rectFocus.Inflate(-2, -2);
+
+                // Draw the focus visuals on the control.
+                e.Graphics.DrawRectangle(this.focusPenBackground, rectFocus);
+                e.Graphics.DrawRectangle(this.focusPenForeground, rectFocus);
+            }
+        }
+    }
+}
