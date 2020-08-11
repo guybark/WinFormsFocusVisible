f78b627be90ba3f6bff2904a3cf2fe177eeb84da
Guy Barker (CELA) <guybark@microsoft.com>
2020-08-11 16:36:47 -0700

    Suppress showing of default keyboard focus feedback.

--- WinFormsFocusVisible/ControlMixForm.cs
+++ WinFormsFocusVisible/ControlMixForm.cs
@@ -26,56 +26,182 @@ namespace WinFormsFocusVisible
             this.Close();
         }
 
-        public void DrawCustomFocusFeedback(Control control, PaintEventArgs e)
+        // Called in response to a custom control gettting WM_UPDATEUISTATE.
+        public bool ShouldProcessUpdateUIState(ref Message m)
+        {
+            bool shouldProcessUpdateUIState = true;
+
+            // Is the focus indicator amonsgt the affected UI state elements?
+            int affectedStates = ((int)m.WParam >> 0x10);
+            if ((int)(affectedStates & NativeMethods.UISF_HIDEFOCUS) == NativeMethods.UISF_HIDEFOCUS)
+            {
+                // Remove the focus indicator from thet set of affected UI elements.
+                int remainingStates = (affectedStates & ~NativeMethods.UISF_HIDEFOCUS);
+
+                // Are any other UI states still affected?
+                if (remainingStates != 0)
+                {
+                    // Remove the focus indicator from the set of affected states,
+                    // and continue to process the messages for those UI states.
+                    m.WParam = (IntPtr)((int)remainingStates << 0x10) + ((int)m.WParam & 0xFF);
+                }
+                else
+                {
+                    // The only UI state changing was to be the focus indicator,
+                    // and we are intentionally suppressing that.
+                    shouldProcessUpdateUIState = false;
+                }
+            }
+
+            return shouldProcessUpdateUIState;
+        }
+
+        public void DrawCustomFocusFeedback(
+            Control control, 
+            PaintEventArgs e,
+            Rectangle offsetRect)
         {
             if (control.Focused)
             {
-                // This is hard coded to look ok on Buttons.
-                Rectangle rectFocus = new Rectangle(
-                    3, 3, control.Width - 6, control.Height - 6);
-
-                e.Graphics.DrawRectangle(this.focusPen, rectFocus);
+                e.Graphics.DrawRectangle(this.focusPen, offsetRect);
             }
         }
     }
 
     public class ButtonWithFocusFeedback : Button
     {
+        private ControlMixForm owningForm;
+
+        private ControlMixForm GetOwningForm()
+        {
+            if (this.owningForm == null)
+            {
+                this.owningForm = this.FindForm() as ControlMixForm;
+            }
+
+            return this.owningForm;
+        }
+
+        protected override void WndProc(ref Message m)
+        {
+            if ((m.Msg != NativeMethods.WM_UPDATEUISTATE) ||
+                GetOwningForm().ShouldProcessUpdateUIState(ref m))
+            {
+                base.WndProc(ref m);
+            }
+        }
+
         protected override void OnPaint(PaintEventArgs e)
         {
             base.OnPaint(e);
 
-            (this.FindForm() as ControlMixForm).DrawCustomFocusFeedback(this, e);
+            GetOwningForm().DrawCustomFocusFeedback(this, e,
+                new Rectangle(1, 1, this.Width - 2, this.Height - 2));
         }
     }
 
     public class CheckBoxWithFocusFeedback : CheckBox
     {
+        private ControlMixForm owningForm;
+
+        private ControlMixForm GetOwningForm()
+        {
+            if (this.owningForm == null)
+            {
+                this.owningForm = this.FindForm() as ControlMixForm;
+            }
+
+            return this.owningForm;
+        }
+
+        protected override void WndProc(ref Message m)
+        {
+            if ((m.Msg != NativeMethods.WM_UPDATEUISTATE) ||
+                GetOwningForm().ShouldProcessUpdateUIState(ref m))
+            {
+                base.WndProc(ref m);
+            }
+        }
+
         protected override void OnPaint(PaintEventArgs e)
         {
             base.OnPaint(e);
 
-            (this.FindForm() as ControlMixForm).DrawCustomFocusFeedback(this, e);
+            GetOwningForm().DrawCustomFocusFeedback(this, e,
+                new Rectangle(1, 1, this.Width - 2, this.Height - 4));
         }
     }
 
     public class RadioButtonithFocusFeedback : RadioButton
     {
+        private ControlMixForm owningForm;
+
+        private ControlMixForm GetOwningForm()
+        {
+            if (this.owningForm == null)
+            {
+                this.owningForm = this.FindForm() as ControlMixForm;
+            }
+
+            return this.owningForm;
+        }
+
+
+        protected override void WndProc(ref Message m)
+        {
+            if ((m.Msg != NativeMethods.WM_UPDATEUISTATE) ||
+                GetOwningForm().ShouldProcessUpdateUIState(ref m))
+            {
+                base.WndProc(ref m);
+            }
+        }
+
         protected override void OnPaint(PaintEventArgs e)
         {
             base.OnPaint(e);
 
-            (this.FindForm() as ControlMixForm).DrawCustomFocusFeedback(this, e);
+            GetOwningForm().DrawCustomFocusFeedback(this, e,
+                new Rectangle(1, 1, this.Width - 2, this.Height - 2));
         }
     }
 
     public class LinkLabelWithFocusFeedback : LinkLabel
     {
+        private ControlMixForm owningForm;
+
+        private ControlMixForm GetOwningForm()
+        {
+            if (this.owningForm == null)
+            {
+                this.owningForm = this.FindForm() as ControlMixForm;
+            }
+
+            return this.owningForm;
+        }
+
+
+        protected override void WndProc(ref Message m)
+        {
+            if ((m.Msg != NativeMethods.WM_UPDATEUISTATE) ||
+                GetOwningForm().ShouldProcessUpdateUIState(ref m))
+            {
+                base.WndProc(ref m);
+            }
+        }
+
         protected override void OnPaint(PaintEventArgs e)
         {
             base.OnPaint(e);
 
-            (this.FindForm() as ControlMixForm).DrawCustomFocusFeedback(this, e);
+            GetOwningForm().DrawCustomFocusFeedback(this, e,
+                new Rectangle(1, 1, this.Width - 2, this.Height - 2));
         }
     }
+
+    internal static class NativeMethods
+    {
+        public const int WM_UPDATEUISTATE = 0x0128;
+        public const int UISF_HIDEFOCUS = 0x01;
+    }
 }
+
